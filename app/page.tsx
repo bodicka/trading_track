d59ad8a34d2405dc50668bedca05461af698c5fc
bodicka <ms.s1506@mail.ru>
2025-10-24 01:45:26 +0300

    Initial comit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button className="cursor-pointer">Click Me</Button>
+    </div>
+  )
+}
+
+export default Home;
