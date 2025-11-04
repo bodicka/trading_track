1be907f6cd876186e3e150afd0f4ab83df38771f
bodicka <ms.s1506@mail.ru>
2025-11-04 19:36:01 +0300

    Change to sign-in

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -36,9 +36,11 @@ const SignIn = () => {
           register={register}
           error={errors.email}
           validation={{
-            require: "Email name is required",
-            pattern: /^\w+@\w+\.\w+$/,
-            message: "Email address is required",
+            required: "Email name is required",
+            pattern: {
+              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+              message: "Enter a valid email address",
+            },
           }}
         />
         <InputFill
@@ -48,7 +50,13 @@ const SignIn = () => {
           type="password"
           register={register}
           error={errors.password}
-          validation={{ require: "Password is required", minLength: 8 }}
+          validation={{
+            required: "Password is required",
+            minLength: {
+              value: 8,
+              message: "Password must be at least 8 characters long",
+            },
+          }}
         />
         <Button
           type="submit"
@@ -59,7 +67,7 @@ const SignIn = () => {
         </Button>
         <FooterLink
           text="Don’t have an accoun"
-          href="sign-up"
+          href="/sign-up"
           linkText="Sign Up"
         />
       </form>
